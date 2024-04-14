5a266aba784d1412a270cee38d197af0748e4c27
Fayssal Mechmeche <fayssalmechmeche.pro@gmail.com>
2024-04-14 17:52:14 +0200

    fix entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,12 +8,13 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: 'user')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(name: 'id')]
+    #[ORM\Column]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
